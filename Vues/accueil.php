1adcf5495d4e7360e9a4976c1563ac7336fd1772
sewelseb <sewelseb@hotmail.com>
2015-07-02 00:14:41 +0200

    video facebook et remove le logo

--- Vues/accueil.php
+++ Vues/accueil.php
@@ -22,6 +22,16 @@
 </head>
 
 <body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+	<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3&appId=288222224714486";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+		</script>
+
 	<!-- Preloader -->
 	<div id="preloader">
 	  <div id="load"></div>
@@ -64,8 +74,21 @@
     <section id="intro" class="intro">
 	
 		<div class="slogan">
+			<video autoplay loop poster="polina.jpg" id="bgvid">
+
+				<!-- <source src="polina.webm" type="video/webm"> -->
+
+				<source src="polina.mp4" type="video/mp4">
+
+			</video>
 			<h2>WELCOME TO <span class="text_color">SMILE</span> </h2>
-			<h4>We promote paintball, we shoot your craziest Scenario/Big Game events to produce action videos to share to the world ! SMILE !</h4>
+			
+					<h4 class="largeurMax">
+						We are Media & Paintball Team.<br/>
+						We promote the sport through videos of Scenario Big Games, players, teams and industry actors we media-cover for the world.
+					</h4>
+				
+			
 		</div>
 		<div class="page-scroll">
 			<a href="#theTeam" class="btn btn-circle">
@@ -213,101 +236,68 @@
 				<hr class="marginbot-50">
 			</div>
 		</div>
-       <div class="row">
-				<div class="col-xs-6 col-md-4">
-					<div class="wow fadeInUp" data-wow-delay="0.2s">
-	                <div class="service-box">
-						<div class="service-icon">
-							<img src="img/icons/service-icon-3.png" alt="" />
-						</div>
-						<div class="service-desc">
-							<h5>SMILE Paintball Network - Paintball Dragon Tournament</h5>
-							<div class="embed-container">
-								<iframe  src="https://www.youtube.com/embed/o1Oi_Yo4xL4" frameborder="0" allowfullscreen></iframe>
-							</div>
-						</div>
-	                </div>
-					</div>
-	            </div>
-				<div class="col-xs-6 col-md-4">
-					<div class="wow fadeInRight" data-wow-delay="0.4s">
-	                <div class="service-box">
-						<div class="service-icon">
-							<img src="img/icons/service-icon-4.png" alt="" />
-						</div>
-						<div class="service-desc">
-							<h5>SMILE Paintball Network : Fort-Paint Castle Game </h5>
-							<div class="embed-container">
-								<iframe  src="https://www.youtube.com/embed/krMENaCoMaE" frameborder="0" allowfullscreen></iframe>
-							</div>
-						</div>
-	                </div>
-					</div>
-	            </div>
-	            <div class="col-xs-6 col-md-4">
-					<div class="wow fadeInRight" data-wow-delay="0.6s">
-	                <div class="service-box">
-						<div class="service-icon">
-							<img src="img/icons/service-icon-4.png" alt="" />
-						</div>
-						<div class="service-desc">
-							<h5>SMILE Paintball Network : Shoreline Call To Duty V</h5>
-							<div class="embed-container">
-								<iframe  src="https://www.youtube.com/embed/iZCDhhExW7E" frameborder="0" allowfullscreen></iframe>
-							</div>
-						</div>
-	                </div>
-					</div>
-	            </div>
+		<div class="row wow bounceInUp" data-wow-delay="0.2s">
+			<div class="fb-video" data-href="https://www.facebook.com/smilepaintball/videos/vb.1552858474930301/1623823687833779/?type=2&amp;theater" data-width="500"><div class="fb-xfbml-parse-ignore"><blockquote cite="/smilepaintball/videos/1623823687833779/"><a href="/smilepaintball/videos/1623823687833779/">SMILE Paintball Network : France VS Belgique 2015 : 150VS150</a><p>Follow our page SMILE Paintball Network and our Youtube channel to see more action videos. Suivez notre page pour voir plus de vidéos d&#039;action. https://youtu.be/3GFKRLPlqC0?list=PLvVNq9eaEuXtFhIRp2ZQHtfQC-gsBdgTh
 
-	            <div class="col-xs-6 col-md-4">
-					<div class="wow fadeInUp" data-wow-delay="0.8s">
-	                <div class="service-box">
-						<div class="service-icon">
-							<img src="img/icons/service-icon-3.png" alt="" />
-						</div>
-						<div class="service-desc">
-							<h5>SMILE Paintball Network - Feb 2015 Teaser</h5>
-							<div class="embed-container">
-								<iframe  src="https://www.youtube.com/embed/_Eszyp-JwwA" frameborder="0" allowfullscreen></iframe>
-							</div>
-						</div>
-	                </div>
-					</div>
-	            </div>
-				<div class="col-xs-6 col-md-4">
-					<div class="wow fadeInRight" data-wow-delay="1.0s">
-	                <div class="service-box">
-						<div class="service-icon">
-							<img src="img/icons/service-icon-4.png" alt="" />
-						</div>
-						<div class="service-desc">
-							<h5>SMILE Paintball Network - BIG FIGHT Acting Paintball Janv 2015</h5>
-							<div class="embed-container">
-								<iframe  src="https://www.youtube.com/embed/Y5EYqt_9LJc" frameborder="0" allowfullscreen></iframe>
-							</div>
-						</div>
-	                </div>
-					</div>
-	            </div>
-	            <div class="col-xs-6 col-md-4">
-					<div class="wow fadeInRight" data-wow-delay="1.2s">
-	                <div class="service-box">
-						<div class="service-icon">
-							<img src="img/icons/service-icon-4.png" alt="" />
-						</div>
-						<div class="service-desc">
-							<h5>SMILE Paintball Network : The Big Game Network </h5>
-							<div class="embed-container">
-								<iframe  src="https://www.youtube.com/embed/YfJZzeIXi-E" frameborder="0" allowfullscreen></iframe>
-							</div>
-						</div>
-	                </div>
-					</div>
-	            </div>
+			SMILE Paintball Network remercie Iron Balls Couvin Page Iron Balls Couvin pour nous avoir reçus à l&#039;occasion de ce FRANCE VS BELGIQUE 2015.
+			Nos sponsors :
+			Red Bull pour l&#039;énergie, Stock Americain Sprl Van Ryckel pour les uniformes et équipement tactique, patch-werk pour les patchs de teams les plus cool, KG-Factory pour les adapteurs MagFed universels, rails TIPX et nombres d&#039;accessoires innovateurs paintball imprimés en 3D.
+			Ainsi que toutes les Teams, joueurs, Staff et la SMILE Vidéo Team, qui ont contribué à ce France VS Belgique mémorable !</p>Posted by <a href="https://www.facebook.com/smilepaintball">SMILE Paintball Network</a> on samedi 6 juin 2015</blockquote></div></div>
+		</div>
+		<div class="col-lg-2 col-lg-offset-5">
+				<hr class="marginbot-50">
+			</div>
+       <div class="row wow bounceInUp" data-wow-delay="0.4s">
+			<div class="fb-video" data-href="https://www.facebook.com/smilepaintball/videos/vl.674752066004423/1609629515919863/?type=1&amp;theater" data-width="500"><div class="fb-xfbml-parse-ignore"><blockquote cite="/smilepaintball/videos/1609629515919863/"><a href="/smilepaintball/videos/1609629515919863/">SMILE Paintball Network - Paintball Dragon Tournament</a><p>SMILE Paintball Network remercie Paintball Dragon  et Black Dragons Paintball Team pour l&#039;organisation.
+
+			Our official Sponsors / Les sponsors officiels de SMILE Paintball Network :
+			Red Bull pour l&#039;énergie
+			Stock Americain Sprl Van Ryckel pour les uniformes et équipement tactique 
+			patch-werk pour les patchs de teams les plus cool
+			KG-Factory pour les adapteurs MagFed universels et nombres d&#039;accessoires innovateurs paintball imprimés en 3D.
+
+			N&#039;hésitez pas à liker et partager notre page pour plus d&#039;action en vidéo https://www.facebook.com/smilepaintball
+
+			https://www.youtube.com/channel/UC8XVTKNss5yDKaap67BcUcg</p>Posted by <a href="https://www.facebook.com/smilepaintball">SMILE Paintball Network</a> on mardi 21 avril 2015</blockquote></div></div>	
 
 
-        	</div>
+		</div>
+		<div class="col-lg-2 col-lg-offset-5">
+				<hr class="marginbot-50">
+			</div>
+		<div class="row wow bounceInUp" data-wow-delay="0.2s">
+			<div class="fb-video" data-href="https://www.facebook.com/smilepaintball/videos/vl.674752066004423/1599712093578272/?type=1&amp;theater" data-width="500"><div class="fb-xfbml-parse-ignore"><blockquote cite="/smilepaintball/videos/1599712093578272/"><a href="/smilepaintball/videos/1599712093578272/">SMILE Paintball Network : Fort-Paint Castle Game</a><p>SMILE Paintball Network &#064; FORT-PAINT crazy Castle Game
+				We would like to thank our official sponsors // Nous remercions nos sponsors officiels
+				Stock Americain Sprl Van Ryckel for the uniforms and tactical gear.
+				patch-werk for the coolest team patches
+				Red Bull for the energy
+
+				Like and share our page https://www.facebook.com/smilepaintball
+
+				More Videos on our youtube channel !
+				https://www.youtube.com/channel/UC8XVTKNss5yDKaap67BcUcg</p>Posted by <a href="https://www.facebook.com/smilepaintball">SMILE Paintball Network</a> on lundi 23 mars 2015</blockquote></div></div>
+		</div>
+		<div class="col-lg-2 col-lg-offset-5">
+				<hr class="marginbot-50">
+			</div>
+		<div class="row wow bounceInUp" data-wow-delay="0.2s">
+			<div class="fb-video" data-href="https://www.facebook.com/smilepaintball/videos/vl.674752066004423/1615258632023618/?type=1&amp;theater" data-width="500"><div class="fb-xfbml-parse-ignore"><blockquote cite="/smilepaintball/videos/1615258632023618/"><a href="/smilepaintball/videos/1615258632023618/">SMILE Paintball Network - Big Fight Loader Vintage Style &#064; A...</a><p>SMILE Paintball Network remercie Acting Paintball pour nous avoir reçus.
+			Big Fight Loader Vintage Style.
+
+			Our Sponsors / Nos sponsors :
+			Red Bull pour l&#039;énergie
+			Stock Americain Sprl Van Ryckel pour les uniformes et équipement tactique 
+			patch-werk pour les patchs de team les plus cool
+			KG-Factory pour les adapteurs MagFed universels et nombres d&#039;accessoires innovateurs paintball imprimés en 3D.
+
+			N&#039;hésitez pas à suivre et partager notre page pour plus d&#039;action en vidéo
+			Follow and share our page to see more action videos https://www.facebook.com/smilepaintball
+
+			https://www.youtube.com/channel/UC8XVTKNss5yDKaap67BcUcg</p>Posted by <a href="https://www.facebook.com/smilepaintball">SMILE Paintball Network</a> on mardi 12 mai 2015</blockquote></div></div>
+		</div>
+		<div class="col-lg-2 col-lg-offset-5">
+				<hr class="marginbot-50">
+			</div>
         	<div class="row">
         		<h1>&nbsp;</h1>
         	</div>
